a1cd11ad6f0680766cd34e1af681bd1d77ef2e47
53ningen <gomi2ngen@gmail.com>
2024-12-31 04:25:42 +0900

    support tag base revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidatePath } from 'next/cache'
+import { revalidatePath, revalidateTag } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 const { API_KEY } = process.env
@@ -10,9 +10,16 @@ export async function PUT(request: NextRequest) {
   }
   const searchParams = request.nextUrl.searchParams
   const p = searchParams.get('path')
-  if (!p) {
-    return NextResponse.json({ status: 'failed', message: 'invalid request' }, { status: 400 })
+  if (p) {
+    revalidatePath(p)
+    return NextResponse.json({ status: 'success' })
   }
-  revalidatePath(p)
-  return NextResponse.json({ status: 'success' })
+
+  const t = searchParams.get('tag')
+  if (t) {
+    revalidateTag(t)
+    return NextResponse.json({ status: 'success' })
+  }
+
+  return NextResponse.json({ status: 'failed', message: 'invalid request' }, { status: 400 })
 }
